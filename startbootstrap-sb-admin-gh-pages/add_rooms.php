bd50d1be94a41d94060aafa60d51118f8e52aae0
Mishra-Vinaykumar <vinaymishra9925@gmail.com>
2024-01-02 16:13:16 +0530

    adding admin panel,rooms,mess, Data see

---
+++ startbootstrap-sb-admin-gh-pages/add_rooms.php
@@ -0,0 +1,231 @@
+<?php error_reporting(0) ?>
+<?php include "conn.php" ?>
+<?php
+
+$q = "SELECT * FROM rooms";
+$result = mysqli_query($conn,$q) or die('Query faild');
+    
+if (mysqli_num_rows($result) > 0) {
+     
+?>
+  
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Hostel mess Menu</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="index.php">Start Bootstrap</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+                class="fas fa-bars"></i></button>
+        <!-- Navbar Search-->
+        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..."
+                    aria-describedby="btnNavbarSearch" />
+                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i
+                        class="fas fa-search"></i></button>
+            </div>
+        </form>
+        <!-- Navbar-->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#!">Settings</a></li>
+                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                    <li>
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li><a class="dropdown-item" href="#!">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Core</div>
+                        <a class="nav-link" href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Dashboard
+                        </a>
+                        <div class="sb-sidenav-menu-heading">Interface</div>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                            Layouts
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="add_rooms.php">Adding Rooms</a>
+                                <a class="nav-link" href="hostel_mess_menu.php">Hostel Mess </a>
+                            </nav>
+                        </div>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages"
+                            aria-expanded="false" aria-controls="collapsePages">
+                            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                            Pages
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                                    data-bs-target="#pagesCollapseAuth" aria-expanded="false"
+                                    aria-controls="pagesCollapseAuth">
+                                    Authentication
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                </a>
+                                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
+                                    data-bs-parent="#sidenavAccordionPages">
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="login.html">Login</a>
+                                        <a class="nav-link" href="register.html">Register</a>
+                                        <a class="nav-link" href="password.html">Forgot Password</a>
+                                    </nav>
+                                </div>
+                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                                    data-bs-target="#pagesCollapseError" aria-expanded="false"
+                                    aria-controls="pagesCollapseError">
+                                    Error
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                </a>
+                                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
+                                    data-bs-parent="#sidenavAccordionPages">
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="401.html">401 Page</a>
+                                        <a class="nav-link" href="404.html">404 Page</a>
+                                        <a class="nav-link" href="500.html">500 Page</a>
+                                    </nav>
+                                </div>
+                            </nav>
+                        </div>
+                        <div class="sb-sidenav-menu-heading">Addons</div>
+                        <a class="nav-link" href="charts.html">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Charts
+                        </a>
+                        <a class="nav-link" href="tables.html">
+                            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                            Tables
+                        </a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Start Bootstrap
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Sidenav Light</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                    </ol>
+                    <div class="card mb-4">
+                        <div class="card-body">
+    
+                            <a class="btn btn-success btn-lg float-end" href="add_new_rooms.php" role="button">Add Rooms</a>
+
+                            <h3>Hostel Mess Menu</h3>
+                        </div>
+ <!-- ------------------------------------------------------------- -->
+
+ <table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Collge</th>
+      <th scope="col">Rooms</th>
+      <th scope="col">facility</th>
+      <th scope="col">Hostel Type</th>
+      <th scope="col">Building</th>
+      <th scope="col">Apply Fees</th>
+      <th scope="col">Status</th>
+      <th scope="col" colspan="2">Action</th>
+    </tr>
+  </thead>
+
+  <?php  while ($row = mysqli_fetch_assoc($result)) { ?>
+
+  <tbody>
+    <tr>
+      <th scope="row"><?php echo $row['id']  ?></th>
+      <td><?php echo $row['clg']             ?></td>
+      <td><?php echo $row['rooms']           ?></td>
+      <td><?php echo $row['facility']        ?></td>
+      <td><?php echo $row['hosteltype']      ?></td>
+      <td><?php echo $row['building']        ?></td>
+      <td><?php echo $row['applyfess']       ?></td>
+      <td><?php echo $row['status']          ?></td>
+      <td><button type="button" class="btn btn-outline-success">Update</button></td>
+      <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+        <td><input type="submit" class="btn btn-outline-danger" name="delete" value="Delete"></td>
+    </form>
+    </tr>
+    <?php } ?>
+  </tbody>
+</table>
+
+<?php 
+}
+?>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
+
+<?php
+
+// if (isset($_POST['delete'])) {
+//     $id = $_POST['id'];
+
+//     $delete = "SELECT * FROM rooms WHERE id = '$id' ";
+//     $del_result = mysqli_query($conn,$delete) or die("Unsucess Full");
+
+//     if ($del_result) {
+//         echo "Success";
+//     }else {
+//         echo "not Working";
+//     }
+// }
+?>
---
+++ startbootstrap-sb-admin-gh-pages/backend_add_rooms.php
@@ -0,0 +1,29 @@
+<?php 
+include "conn.php";
+
+if (isset($_POST['Submit'])) {
+
+   $clg = $_POST['clg'];
+   $rooms = $_POST['rooms']; 
+   $facility = $_POST['facility'];
+   $hosteltype = $_POST['hosteltype'];
+   $buildingName = $_POST['building'];
+   $applyfess = $_POST['applyfess'];
+   $status = $_POST['status'];
+
+   $query = "INSERT INTO rooms (clg,rooms,facility,hosteltype,building,applyfess,status) VALUES ('{$clg}','{$rooms}','{$facility}','{$hosteltype}','{$buildingName}','{$applyfess}','{$status}')" or die("query fail");
+
+   $result= mysqli_query($conn,$query) or die("Query fails");
+
+   if ($result) {
+      echo "<script> alert('Data inserted sucseesfully') </script>";
+      header("Location: add_rooms.php");
+   }else {
+      echo "<script> alert('Not Insert or Failed') </script>";
+      // header("Location:");
+   }
+
+
+
+}
+?>
